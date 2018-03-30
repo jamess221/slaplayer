99cf9a1484f4cd0af73a5a765f3821d4365e8941
jamess221 <James.sheard221@gmail.com>
2018-03-30 20:48:33 +0100

    basics

--- config/database.php
+++ config/database.php
@@ -40,18 +40,15 @@ return [
         ],
 
         'mysql' => [
-            'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'strict' => true,
-            'engine' => null,
+            'driver'    => 'mysql',
+            'host'      => env('DB_HOST', 'homestead' == gethostname() ? 'localhost' : '127.0.0.1'),
+            'database'  => env('DB_DATABASE', 'forge'),
+            'username'  => env('DB_USERNAME', 'forge'),
+            'password'  => env('DB_PASSWORD', ''),
+            'port'      => env('DB_PORT', 'homestead' == gethostname() ? null : 33060),
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => ''
         ],
 
         'pgsql' => [
